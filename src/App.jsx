06ae849ad8ea007e46bd8200037240724572cdd4
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2024-01-18 21:43:25 +0600

    add2

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ function App() {
     <div className="max-w-screen-2xl md:w-full lg:p-1 p-6 mx-auto mt-16">
       {/* header here */}
       <header className="lg:flex justify-between ">
-        <img src="/src/assets/Component 40.png" alt="logo"/>
-        <img src="/src/assets/Frame 3.png" alt="function"/>
+        <img src="https://i.ibb.co/ZJJxtfz/Component-40.png" alt="logo"/>
+        <img src="https://i.ibb.co/NZdRZ6M/Frame-3.png" alt="function"/>
       </header>
       {/* sign here */}
       <div>
-        <img className="mt-8" src="/src/assets/Frame 47.png" alt="sign"/>
+        <img className="mt-8" src="https://i.ibb.co/5B85KQZ/Frame-47.png" alt="sign"/>
       </div>
       {/* subject here */}
       <div>
-        <img className="mt-8" src="/src/assets/Relations and Functions ( Mathematics ).png" alt="subject"/>
+        <img className="mt-8" src="https://i.ibb.co/DprWH7t/Relations-and-Functions-Mathematics.png" alt="subject"/>
       </div>
       {/* slider here */}
       <div className="grid justify-items-center">
-        <img className="mt-16" src="/src/assets/title 01.png" alt="title"/>
-        <img className="mt-4" src="/src/assets/Frame 44.png" alt="slider"/>
+        <img className="mt-16" src="https://i.ibb.co/MsLgRK6/title-01.png" alt="title"/>
+        <img className="mt-4" src="https://i.ibb.co/ySpVt1K/Frame-44.png" alt="slider"/>
       </div>
       {/* playlist */}
        <div className="mt-8 lg:flex justify-between items-center">
-       <img className="mt-16" src="/src/assets/Frame 41.png" alt="logo"/>
-        <img className="mt-4 md:w-50 h-16" src="/src/assets/Frame 48.png" alt="playlist"/>
+       <img className="mt-16" src="https://i.ibb.co/1rNLLxv/Frame-41.png" alt="logo"/>
+        <img className="mt-4 md:w-50 h-16" src="https://i.ibb.co/f948KnF/Frame-48.png" alt="playlist"/>
        </div>
       <div className="mb-8">
        {/* FAQ */}
-       <img className="mt-16" src="/src/assets/FAQ.png" alt="faq"/>
-        <img className="mt-8 lg:w-2/3" src="/src/assets/Frame 49.png" alt="question"/>
+       <img className="mt-16" src="https://i.ibb.co/2NLShKB/FAQ.png" alt="faq"/>
+        <img className="mt-8 lg:w-2/3" src="https://i.ibb.co/7nctZPv/Frame-49.png" alt="question"/>
 
       </div>
     </div>
